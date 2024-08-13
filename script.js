3f779d56ec4c8bd39903d9f3b6fd51d9cb73b9f6
marshallartsdigital <media@marshall-arts.co.uk>
2024-08-13 16:21:55 +0100

    SPC site

---
+++ script.js
@@ -0,0 +1,4 @@
+function buyTickets() {
+    // Redirect to ticket purchase page or handle ticket purchase
+    window.location.href = 'https://www.ticketwebsite.com'; // Replace with actual ticket purchase URL
+}
