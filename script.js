6718ec9b1fa5a6f6f43e7f9a8270cb53d7b18437
marshallartsdigital <media@marshall-arts.co.uk>
2024-08-13 17:43:07 +0100

    SPC

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function buyTickets() {
     // Redirect to ticket purchase page or handle ticket purchase
-    window.location.href = 'https://www.ticketwebsite.com'; // Replace with actual ticket purchase URL
+    window.location.href = 'https://marshall-arts.com/current-tours'; // Replace with actual ticket purchase URL
 }
